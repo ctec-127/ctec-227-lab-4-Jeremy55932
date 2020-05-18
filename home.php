50d22ddb3db3052169108133a2e69e6e722da4a7
Jeremy55932 <j.lo4@students.clark.edu>
2020-05-18 13:36:44 -0700

    completed

--- home.php
+++ home.php
@@ -3,13 +3,11 @@
 session_start();
 $pageTitle = 'Home';
 require 'inc/header.inc.php';
-// $_SESSION['folder'] = 'username';
 require 'inc/display.inc.php';
-// include 'register.php';
 
 ?>
 <header>
-    <ul>
+    <ul class="homepage">
         <li>
             <a href="register.php" id="register">Register</a>
         </li>
@@ -20,21 +18,24 @@ require 'inc/display.inc.php';
             <a href="" id="logout">Logout</a>
         </li>
     </ul>
-	<h1><?= isset($_SESSION['first_name']) ? $_SESSION['first_name'] . "'s File Uploads": 'Online File Uploader' ?></h1>
+    
+<h1><?= isset($_SESSION['first_name']) ? $_SESSION['first_name'] . "'s File Uploads": 'Online File Uploader' ?></h1>
+<ul class="home">
+      <li>
+        <a href="home.php" id="home">Home</a>
+      </li>
+    </ul>
 </header>
 
 <h1 class="welcome">Welcome to our great site <?= isset($_SESSION['first_name']) ? $_SESSION['first_name'] : 'where you can upload all your images into your own personal online account' ?></h1>
-<!-- <div id="message"></div> -->
 
 <?php
-	require 'inc/footer.inc.php';
 	include "inc/error.inc.php";
     include "inc/delete.inc.php";
     include 'inc/logged-in-form.inc.php';
-
 	display_images();
+	require 'inc/footer.inc.php';
 	?>
-<!-- <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
+<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script defer src="js/script.js"></script>
 
-<?php require 'inc/footer.inc.php' ?>
